f4d06a5db024a2a426c56096a0f0a1431bb4d066
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-09-07 20:58:37 +0200

    Step6 : Client - Program

--- IteratorDesignPattern/Program.cs
+++ IteratorDesignPattern/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IteratorDesignPattern;
+
+ConcreteCollection collection = new ConcreteCollection();
+collection.AddEmployee(new Employee("Anurag", 100));
+collection.AddEmployee(new Employee("Pranaya", 101));
+collection.AddEmployee(new Employee("Santosh", 102));
+collection.AddEmployee(new Employee("Priyanka", 103));
+collection.AddEmployee(new Employee("Abinash", 104));
+collection.AddEmployee(new Employee("Preety", 105));
+
+
+Iterator iterator = collection.CreateIterator();
+    
+Console.WriteLine("Iterating over collection:");
+
+for (Employee emp = iterator.First(); !iterator.IsCompleted; emp = iterator.Next())
+{
+    Console.WriteLine($"ID : {emp.ID} & Name : {emp.Name}");
+}
+
+Console.Read();
