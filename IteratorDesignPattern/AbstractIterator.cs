74f4bfe7d03ab72a06b504506a8c77b72e30c373
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-09-07 20:44:21 +0200

    Step2: Creating Abstract Iterator

---
+++ IteratorDesignPattern/AbstractIterator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorDesignPattern
+{
+    interface AbstractIterator
+    {
+        interface AbstractIterator
+        {
+            Employee First();
+            Employee Next();
+            bool IsCompleted { get; }
+        }
+    }
+}
