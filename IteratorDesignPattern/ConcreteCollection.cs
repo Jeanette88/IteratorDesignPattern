bd3b3b3af97aebf550a4909540db618bf9f59dc6
Jeanette Sonnerborg <jv_88_@hotmail.com>
2022-09-07 20:55:54 +0200

    Step5: Creating ConcreteAggregate

---
+++ IteratorDesignPattern/ConcreteCollection.cs
@@ -0,0 +1,27 @@
+﻿namespace IteratorDesignPattern
+{
+    class ConcreteCollection : AbstractIterator
+    {
+        private List<Employee> listEmployees = new List<Employee>();
+     
+        public Iterator CreateIterator()
+        {
+            return new Iterator(this);
+        }
+      
+        public int Count
+        {
+            get { return listEmployees.Count; }
+        }
+       
+        public void AddEmployee(Employee employee)
+        {
+            listEmployees.Add(employee);
+        }
+    
+        public Employee GetEmployee(int IndexPosition)
+        {
+            return listEmployees[IndexPosition];
+        }
+    }
+}
